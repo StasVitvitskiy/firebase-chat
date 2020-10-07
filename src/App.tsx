cdf3f6ee80b216f0bf5d6fb669665b717b09f4af
Stas Vitvitskiy <stanislav.i.vitvitskiy@gmail.com>
2020-10-07 13:31:14 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+// eslint-disable-next-line
+import * as React from "react";
+import styled from "styled-components";
+
+const StyledAppContainer = styled.div`
+  font-family: sans-serif;
+  text-align: center;
+`;
+
+export function App() {
+  return <StyledAppContainer>Header</StyledAppContainer>;
+}
